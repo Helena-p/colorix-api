ab41aac0d177ed3ae4ee256a1f34b4a0c54312dd
Helena <helenasleterkesto@gmail.com>
2023-07-31 18:51:23 +0200

    Add: handle uncaught exceptions and rejections

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+	console.log(err.name, err.message);
+	// shut down application on uncaught exception
+	process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
 	'<PASSWORD>',
 	process.env.DATABASE_PASSWORD
@@ -20,4 +26,12 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+	console.log(err);
+	server.close(() => {
+		// shut down application on uncaught exception
+		process.exit(1);
+	});
 });
