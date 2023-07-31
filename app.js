ef7948163a4a77d235c767aeb2456e599a26a861
Helena <helenasleterkesto@gmail.com>
2023-07-31 17:16:20 +0200

    Add: differentiate error msg on environment

--- app.js
+++ app.js
@@ -14,10 +14,21 @@ app.all('*', (req, _, next) => {
 app.use((err, _, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'error';
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message,
-	});
+
+	if (process.env.NODE_ENV === 'development') {
+		res.status(err.statusCode).json({
+			status: err.status,
+			error: err,
+			message: err.message,
+			stack: err.stack,
+		});
+	} else if (process.env.NODE_ENV === 'production') {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+		});
+	}
+
 	next();
 });
 
