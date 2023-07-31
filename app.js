1d6d2200e7458c18be51d122e013bbc02636ccae
Helena <helenasleterkesto@gmail.com>
2023-07-31 15:12:39 +0200

    Add: global error handler for non-existing routes

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const productRouter = require('./routes/productRoutes');
+const AppError = require('./utils/appError');
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/products', productRouter);
 // handle non-existing routes for all HTTP methods (GET, POST ...)
-app.all('*', (req, res, next) => {
-	res.status(404).json({
-		status: 'fail',
-		message: `Cannot find ${req.originalUrl} on this server`,
+app.all('*', (req, _, next) => {
+	next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+app.use((err, _, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
 	});
 	next();
 });
+
 module.exports = app;
