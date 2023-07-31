af223b4937e3f46b06edc3745fd8a916f4304221
Helena <helenasleterkesto@gmail.com>
2023-07-31 13:17:39 +0200

    Add: handle non-existing routes

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/products', productRouter);
+// handle non-existing routes for all HTTP methods (GET, POST ...)
+app.all('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Cannot find ${req.originalUrl} on this server`,
+	});
+	next();
+});
 module.exports = app;
